129012dc1122f29896fd197aaf5002a6dad1bb1e
King-Hin Leung <>
2023-03-31 13:16:12 +0200

    Add node-fetch to communicate with DY experience API

--- apis/CampaignApi.ts
+++ apis/CampaignApi.ts
@@ -1,22 +1,66 @@
 import {Request} from '@frontastic/extension-types';
+import fetch from 'node-fetch';
 
+const APIKEY = '973cbfa6b49b057f9fead41953899557beb49e14779504c0ee86316d66e8df2d'; // replace with your key
+const DYHOST = 'https://dy-api.com';      // or .eu
 
 export default class CampaignApi {
+
+  async choose(userId, sessionId, dyContext, selectors = []) {
+    const body = {
+      selector: {
+        names: [
+          'HP Hero Banner'
+        ],
+      },
+      user: {
+        id: userId,
+      },
+      session: {
+        custom: sessionId,
+      },
+      context : dyContext
+    }
+
+    const url = `https://dy-api.com/v2/serve/user/choose`
+    const
+      headers=  {
+        'dy-api-key': APIKEY,
+        'Content-Type': 'application/json'
+    }
+
+    let response = {};
+    let resultBody = {}
+    try {
+       response =  await fetch(url, {
+        method: 'post',
+        body : JSON.stringify(body),
+        headers
+      });
+      resultBody = JSON.stringify(await response.json())
+      console.log(`Choose response: ${resultBody}`);
+    } catch (e) {
+      console.log(e)
+      console.error(`ERROR IN CHOOSE: ${e.message}`);
+    }
+    return resultBody;
+  }
+
 
   constructor() {
 
   }
-
 
   getDyContext(req: Request) {
     const dyContext = {
       page: {
         location: `https://${req?.hostname}${req?.path}`,
         referrer: req.headers.referer || '',
+        type : 'homepage',
         data: [],
       },
       device: {
-        userAgent: req?.headers['user-agent'] || '',
+        userAgent: req?.headers.userAgent || '',
         ip: req?.clientIp,
       },
       pageAttributes: req?.query,
--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 import { DataSourceConfiguration, DataSourceContext, ExtensionRegistry } from '@frontastic/extension-types';
 import CampaignApi from './apis/CampaignApi';
+import { v4 as uuidv4 } from 'uuid';
+
 import {getLocale} from "@Content-contentful/utils/Request";
 import { Context, Request } from '@frontastic/extension-types';
 export default {
   'data-sources': {
     'frontastic/dynamicyield/campaign': async (config: DataSourceConfiguration, context: DataSourceContext) => {
+      console.log('campaign datasource test')
       const campaignApi = new CampaignApi();
-
+      const dyContext = campaignApi.getDyContext(context.request);
+      const sessionId = uuidv4();
+      const userId = uuidv4()
+      const selector = [
+          'HP Hero Banner'
+        ]
+      const result = await campaignApi.choose(userId, sessionId, dyContext,selector )
       return {
-        dataSourcePayload: campaignApi.getDyContext(context.request),
+
+        dataSourcePayload: 'test4'
       }
     },
   },
