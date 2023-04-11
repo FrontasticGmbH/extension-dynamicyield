ef8761f1ec59308f45ea532e38c895b09e19386a
King-Hin Leung <>
2023-04-11 12:38:48 +0800

    Add Product Recommendations UI component

--- apis/CampaignApi.ts
+++ apis/CampaignApi.ts
@@ -1,4 +1,5 @@
 import {Request} from '@frontastic/extension-types';
+// @ts-ignore
 import fetch from 'node-fetch';
 
 const APIKEY = '973cbfa6b49b057f9fead41953899557beb49e14779504c0ee86316d66e8df2d'; // replace with your key
@@ -6,12 +7,10 @@ const DYHOST = 'https://dy-api.com';      // or .eu
 
 export default class CampaignApi {
 
-  async choose(userId, sessionId, dyContext, selectors = []) {
+  async choose(userId : any, sessionId : any, dyContext : any, selectors : any[] = []) {
     const body = {
       selector: {
-        names: [
-          'HP Hero Banner'
-        ],
+        names: selectors,
       },
       user: {
         id: userId,
@@ -21,7 +20,8 @@ export default class CampaignApi {
       },
       context : dyContext
     }
-
+    console.log('%%%%%%%%%%%%')
+    console.log(body)
     const url = `https://dy-api.com/v2/serve/user/choose`
     const
       headers=  {
@@ -29,8 +29,8 @@ export default class CampaignApi {
         'Content-Type': 'application/json'
     }
 
-    let response = {};
-    let resultBody = {}
+    let response : any = {};
+    let resultBody :any = {}
     try {
        response =  await fetch(url, {
         method: 'post',
@@ -38,10 +38,9 @@ export default class CampaignApi {
         headers
       });
       resultBody = JSON.stringify(await response.json())
-      console.log(`Choose response: ${resultBody}`);
+      console.log(`================= Choose response: ${resultBody} ===================`);
     } catch (e) {
       console.log(e)
-      console.error(`ERROR IN CHOOSE: ${e.message}`);
     }
     return resultBody;
   }
@@ -52,18 +51,31 @@ export default class CampaignApi {
   }
 
   getDyContext(req: Request) {
+    const headers : Record<string, string> | [] =  req.headers
+
+
+    // TODO : Assign value to referrer and userAgent
+    // const referrer : string | undefined = headers.referrer
+    const referrer : string = ''
+    const userAgent : string = ''
+    const ip : string = req?.clientIp
+    const data : any[] = []
+    const hostname : string = req?.hostname
+    const path : string = req?.path
+    const query : string = req?.query
+
     const dyContext = {
       page: {
-        location: `https://${req?.hostname}${req?.path}`,
-        referrer: req.headers.referer || '',
+        location: `https://${hostname}${path}`,
+        referrer: referrer || '',
         type : 'homepage',
-        data: [],
+        data
       },
       device: {
-        userAgent: req?.headers.userAgent || '',
-        ip: req?.clientIp,
+        userAgent : userAgent || '',
+        ip
       },
-      pageAttributes: req?.query,
+      pageAttributes: query,
     }
     return dyContext
   }
--- index.ts
+++ index.ts
@@ -1,24 +1,20 @@
 import { DataSourceConfiguration, DataSourceContext, ExtensionRegistry } from '@frontastic/extension-types';
 import CampaignApi from './apis/CampaignApi';
-import { v4 as uuidv4 } from 'uuid';
 
-import {getLocale} from "@Content-contentful/utils/Request";
-import { Context, Request } from '@frontastic/extension-types';
 export default {
   'data-sources': {
-    'frontastic/dynamicyield/campaign': async (config: DataSourceConfiguration, context: DataSourceContext) => {
-      console.log('campaign datasource test')
+    'frontastic/dynamicyield/product-recommendations-campaign': async (config: DataSourceConfiguration, context: DataSourceContext) => {
       const campaignApi = new CampaignApi();
       const dyContext = campaignApi.getDyContext(context.request);
-      const sessionId = uuidv4();
-      const userId = uuidv4()
+      const sessionId = context.request?.sessionData?.cartId
+      const userId = context.request?.clientIp
+      const campaignSelectorName = config?.configuration?.campaignSelectorName
       const selector = [
-          'HP Hero Banner'
-        ]
+        campaignSelectorName
+      ]
       const result = await campaignApi.choose(userId, sessionId, dyContext,selector )
       return {
-
-        dataSourcePayload: 'test4'
+        dataSourcePayload: result
       }
     },
   },
