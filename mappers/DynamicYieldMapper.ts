203788647a80bb0fa69950f0141c33ba641409b6
King-Hin Leung <>
2023-05-02 10:13:45 +0800

    Refactor DynamicYieldMapper

--- mappers/DynamicYieldMapper.ts
+++ mappers/DynamicYieldMapper.ts
@@ -1,28 +1,27 @@
-import {Product} from "../../../types/content/dynamicyield/Product";
-import {Money} from "../../../types/content/dynamicyield/Money";
+import { Money } from "../../../types/content/dynamicyield/Money";
+import { Product } from "../../../types/content/dynamicyield/Product";
+import { Slot } from "../../../types/content/dynamicyield/Slot";
 
 export class DynamicYieldMapper {
-
   static mapChooseResponseToProducts(result : any): Product[] {
-    const products = []
+    const products : any = []
     const resultJson = JSON.parse(result)
-    const variation = resultJson.choices[0]?.variations[0]
+    const variation = resultJson?.choices[0]?.variations[0]
     const slots = variation?.payload?.data?.slots
-
-    slots.forEach(item => {
+    slots.forEach((slotItem : Slot) => {
       const price : Money = {
         fractionDigits : 2,
-        centAmount: item?.productData?.price
+        centAmount: slotItem?.productData?.price
       }
-
+      
       const product : Product = {
-        sku : item?.sku,
-        name: item?.productData?.name,
-        description: item?.productData?.description,
-        imageUrl: item?.productData?.image_url,
-        categories: item?.productData?.categories,
-        productType: item?.productData?.group_id,
-        price: price
+        sku : slotItem?.sku,
+        name: slotItem?.productData?.name,
+        description: slotItem?.productData?.description,
+        imageUrl: slotItem?.productData?.image_url,
+        categories: slotItem?.productData?.categories,
+        productType: slotItem?.productData?.group_id,
+        price : price
       }
       products.push(product)
     })
