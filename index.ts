9747c58117571aa58a8218a3fe957a95a1e8edf3
King-Hin Leung <>
2023-05-25 08:42:07 +0200

    Add Error class and parameters validation

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { DataSourceConfiguration, DataSourceContext, ExtensionRegistry } from '@
 import DynamicYieldApi from './apis/DynamicYieldApi';
 import { Product } from '@Types/product/Product';
 import { getContext } from './utils/Request';
-
+import { ValidationError } from './utils/Errors';
 export default {
   'data-sources': {
     'dynamicyield/product-recommendations-campaign': async (
@@ -10,8 +10,19 @@ export default {
       context: DataSourceContext,
     ) => {
       const userId: string = context.request?.clientIp;
+      if (!userId) {
+        throw new ValidationError({
+          message: `Client IP is not defined in context request ${context.request}`,
+        });
+      }
       const dyApi: DynamicYieldApi = new DynamicYieldApi(context.frontasticContext, userId);
       const pageContextType: string = config?.configuration?.pageContextType;
+      if (!pageContextType) {
+        throw new ValidationError({
+          message: `Page context type is not defined in configuration ${config}`,
+        });
+      }
+
       const dyContext = getContext(context.request, pageContextType);
 
       const campaignSelectorName: string = config?.configuration?.campaignSelectorName;
